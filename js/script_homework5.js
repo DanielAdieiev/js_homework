6873c7dd993fe19c68afb622c8e5631a4a4c6094
Adieiev Daniel <danieladieiev@gmail.com>
2024-03-02 00:19:42 +0200

    The homework 5 is added

--- js/script_homework5.js
+++ js/script_homework5.js
@@ -1,1 +1,18 @@
-
+var measure = prompt ('Please input measure','kg, km, h, USD');
+var quantity = prompt ('Please input value');
+    switch (measure){
+        case 'km':
+            console.log (quantity + ' km це ' + quantity * 1000 + ' m')
+            break;
+        case 'kg':
+            console.log (quantity + ' kg це ' + quantity * 1000 + ' g')
+            break;
+        case 'h':
+            console.log (quantity + ' h це ' + quantity * 60 + ' min')
+            break;
+        case 'USD':
+            console.log (quantity + ' USD це ' + quantity * 0.9069 + ' EUR')
+            break;
+        default:
+            console.log ('The measure type is unknown. Please, try again.');
+    } 
