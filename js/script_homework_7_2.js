b25cb36720165c544f92b910506b2c1b8c5533dd
DanielAdieiev <103453967+DanielAdieiev@users.noreply.github.com>
2024-03-09 12:56:53 +0200

    Update script_homework_7_2.js

--- js/script_homework_7_2.js
+++ js/script_homework_7_2.js
@@ -4,10 +4,10 @@ function PadEndOrPadStart (inputString, symbol, quantityOfSymbols, ToTheStart) {
         console.log(inputString);
     }
     else if (ToTheStart == true){
-    console.log(symbol.repeat(quantityOfSymbols - inputString.length) + inputString);
+    console.log(symbol.repeat(symbolsToAdd) + inputString);
     } 
     else if (ToTheStart == false){
-    console.log(inputString + symbol.repeat(quantityOfSymbols - inputString.length));
+    console.log(inputString + symbol.repeat(symbolsToAdd));
     }
 }
 PadEndOrPadStart ('something', '*', 22, 0)
